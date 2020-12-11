2504e8e7859daea7d828e67f157e8ff7546567bd
李育昇 <leonis@manuteq.com>
2020-12-11 11:16:16 +0800

    新增

---
+++ linetest/Controller/LineBotController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Line.Messaging;
+using linetest.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace linetest
+{
+    [Route("api/linebot")]
+    public class LineBotController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly HttpContext _httpContext;
+        private readonly LineBotConfig _lineBotConfig;
+
+        public LineBotController(IServiceProvider serviceProvider,
+            LineBotConfig lineBotConfig)
+        {
+            _httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+            _httpContext = _httpContextAccessor.HttpContext;
+            _lineBotConfig = lineBotConfig;
+        }
+
+        //完整的路由網址就是 https://xxx/api/linebot/run
+        [HttpPost("run")]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var events = await _httpContext.Request.GetWebhookEventsAsync(_lineBotConfig.channelSecret);
+                var lineMessagingClient = new LineMessagingClient(_lineBotConfig.accessToken);
+                var lineBotApp = new LineBotApp(lineMessagingClient);
+                await lineBotApp.RunAsync(events);
+            }
+            catch (Exception ex)
+            {
+                //需要 Log 可自行加入
+                //_logger.LogError(JsonConvert.SerializeObject(ex));
+            }
+            return Ok();
+        }
+    }
+}
---
+++ linetest/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace linetest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureServices((context, services) =>
+            {
+                services.Configure<KestrelServerOptions>(
+                    context.Configuration.GetSection("Kestrel"));
+            }).ConfigureWebHostDefaults(webBuilder =>
+            {
+                //webBuilder.UseHttpSys(options =>
+                //{
+                //    options.UrlPrefixes.Add("http://10.10.10.220:5000");
+                //});
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
