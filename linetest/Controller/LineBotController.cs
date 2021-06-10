11992d0ba6d629603d0bedf4668106c72bdeb2bd
leonis <leonislee0318@gmail.com>
2021-06-10 13:04:14 +0800

    更新

--- linetest/Controller/LineBotController.cs
+++ linetest/Controller/LineBotController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using isRock.LineBot;
 using Line.Messaging;
 using linetest.Helper;
+using linetest.Ｍodels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,7 +15,7 @@ using Newtonsoft.Json;
 namespace linetest
 {
     [Route("api/linebot")]
-    public class LineBotController : Controller
+    public class LineBotController : LineWebHookControllerBase
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly HttpContext _httpContext;
@@ -29,7 +31,108 @@ namespace linetest
         }
         //完整的路由網址就是 https://xxx/api/linebot/run
         [HttpPost("run")]
-        public async Task<IActionResult> Post()
+        public IActionResult Post()
+        {
+            var AdminUserId = "U8dd73688cf9bd43782e74083c6c372f7";
+
+            try
+            {
+                //設定ChannelAccessToken
+                this.ChannelAccessToken = _lineBotConfig.accessToken;
+                //取得Line Event
+
+                var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
+                var userId = LineEvent.source.userId;
+                //配合Line verify 
+                if (LineEvent == null) return Ok();
+                if (LineEvent.replyToken == "00000000000000000000000000000000") return Ok();
+                var UserId = LineEvent.source.userId;
+                var responseMsg = "";
+                //準備回覆訊息
+                if (LineEvent.type.ToLower() == "message")
+                {
+                    if (LineEvent.message.type == "text")
+                    {
+                        var ImgTemplist = MyFun.ImgTemplist();
+                        if (LineEvent.message.text.StartsWith("/"))
+                        {
+                            if (LineEvent.message.text == "/help")
+                            {
+                                responseMsg += "目前可用關鍵字" + Environment.NewLine + string.Join(Environment.NewLine, ImgTemplist.Select(x => x.Key)) + Environment.NewLine + "/選單";
+                                this.ReplyMessage(LineEvent.replyToken, responseMsg);
+                            }
+                            else if (LineEvent.message.text == "/選單")
+                            {
+                                var actions = new List<TemplateActionBase>();
+                                actions.Add(new MessageAction() //MessageAction
+                                {
+                                    label = "倒了",
+                                    text = "已經倒了"
+                                });
+                                actions.Add(new MessageAction() //MessageAction
+                                {
+                                    label = "快倒了",
+                                    text = "看來是快要倒了"
+                                });
+                                actions.Add(new MessageAction() //MessageAction
+                                {
+                                    label = "命不久已",
+                                    text = "唉唉唉"
+                                });
+                                var btn = new isRock.LineBot.ButtonsTemplate
+                                {
+                                    title = "公司倒了嗎",
+                                    text = "我只是想問公司的情況",
+                                    thumbnailImageUrl = new Uri("https://i.imgur.com/mm1JcL0.jpg"),
+                                    actions = actions
+
+
+                                };
+                                this.ReplyMessage(LineEvent.replyToken, btn);
+                                //return Ok();
+                            }
+                            else
+                            {
+                                var ImgTemp = ImgTemplist.Where(x => x.Key == LineEvent.message.text).FirstOrDefault();
+                                if (ImgTemp != null)
+                                {
+                                    this.ReplyMessage(LineEvent.replyToken, ImgTemp.uri);
+                                }
+                                //responseMsg = $"你說了: {LineEvent.message.text}";
+                            }
+                        }
+
+                    }
+                    else if (LineEvent.message.type == "image")
+                    {
+
+
+                    }
+                    else
+                    {
+                        this.ReplyMessage(LineEvent.replyToken, 11538, 51626501);
+                    }
+                }
+                else
+                {
+
+                    //responseMsg = $"收到 event : {LineEvent.type} ";
+                }
+                //回覆訊息
+                //this.ReplyMessage(LineEvent.replyToken, responseMsg);
+                //response OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //回覆訊息
+                //this.PushMessage(AdminUserId, "發生錯誤:\n" + ex.Message);
+                //response OK
+                return Ok();
+            }
+        }
+        [HttpPost("runo")]
+        public async Task<IActionResult> Posto()
         {
             try
             {
---
+++ linetest/Ｍodels/MessageTemp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linetest.Ｍodels
+{
+    public class ImgTemp
+    {
+        /// <summary>
+        /// 關鍵字
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// 圖片路徑
+        /// </summary>
+        public Uri uri { get; set; }
+    }
+    public class MyFun
+    {
+        internal static List<ImgTemp> ImgTemplist()
+        {
+            var list = new List<ImgTemp>();
+            list.Add(new ImgTemp
+            {
+                Key = "/垃圾",
+                uri = new Uri("https://i.imgur.com/mm1JcL0.jpg")
+            });
+            list.Add(new ImgTemp
+            {
+                Key = "/表演",
+                uri = new Uri("https://i.imgur.com/K8SuJIo.jpg")
+            });
+            list.Add(new ImgTemp
+            {
+                Key = "/不早說",
+                uri = new Uri("https://i.imgur.com/wyTqq7Q.gif")
+            });       
+            return list;
+        }
+    }
+}
